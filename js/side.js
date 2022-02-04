ae30be9ae8836e91c788dd270b0f352acfdbcda5
victor1127 <95936106+victor1127@users.noreply.github.com>
2022-02-03 23:11:16 -0400

    Updating the app page

--- js/side.js
+++ js/side.js
@@ -1,4 +1,56 @@
-function DisplayHelloWorld()
+function GetValues()
 {
-    alert("Hello world");
+    let startValue= document.getElementById("startvalue").value;
+    let endValue= document.getElementById("endtvalue").value;
+    let isValidStartValue= IsValidEntry(startValue);
+    let isValidEndValue= IsValidEntry(endValue);
+
+    if(!isValidStartValue || !isValidEndValue)
+    {
+        alert("Enter interger values only");
+        return;
+    }
+
+    let numbers = GetFizzBuzz(startValue, endValue);
+    DisplayResults(numbers);
+
+
 }
+function GetFizzBuzz(value,endValue)
+{
+    let output=[];
+    let message="";
+
+    for(let i=value;i<=endValue;i++)
+    {
+        if(i%3==0) message+="Fizz";
+        if(i%5==0) message+="Buzz";
+        if(message=="") message=i;    
+        
+        output.push(message);
+        message="";
+    }
+
+    return output;
+
+
+}
+function DisplayResults(numbers)
+{
+    let numTemplate="";
+    numbers.forEach(number => {
+
+        numTemplate += `<tr><td>${number}</td></tr>`;
+    
+    });
+
+    document.getElementById("tableresult").innerHTML=numTemplate;
+
+}
+function IsValidEntry(value)
+{
+    let sValue = parseInt(value);
+    return (Number.isInteger(sValue));
+    
+}
+
